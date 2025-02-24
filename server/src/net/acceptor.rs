416dc4a407a1e606d309946baf1f77ff00e20f8f
Finn Bear <finnbearlabs@gmail.com>
2025-02-24 10:26:13 -0800

    0.1.1

--- server/src/net/acceptor.rs
+++ server/src/net/acceptor.rs
@@ -93,12 +93,19 @@ pub fn nodelay_keepalive(stream: &TcpStream, seconds: u64, retries: u32) {
 
     // If I made a mistake and this doesn't work on windows, just remove it ;)
     let sock_ref = SockRef::from(&stream);
-    if let Err(e) = sock_ref.set_tcp_keepalive(
-        &TcpKeepalive::new()
-            .with_time(Duration::from_secs(seconds))
-            .with_interval(Duration::from_secs(seconds))
-            .with_retries(retries),
-    ) {
+    #[cfg_attr(windows, allow(unused_mut))]
+    let mut params = TcpKeepalive::new()
+        .with_time(Duration::from_secs(seconds))
+        .with_interval(Duration::from_secs(seconds));
+    #[cfg(windows)]
+    {
+        let _ = retries;
+    }
+    #[cfg(not(windows))]
+    {
+        params = params.with_retries(retries);
+    }
+    if let Err(e) = sock_ref.set_tcp_keepalive(&params) {
         error!("failed to set TCP keepalive: {e}");
     }
 }
